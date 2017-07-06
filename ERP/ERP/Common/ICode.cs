12eff18dedcbe375f4cbbcd7653090c36ef9f2b1
crobertjones <crjones68@yahoo.com>
2017-07-06 09:50:42 -0400

    Added SCM Package
    Added SCM Package

---
+++ ERP/ERP/Common/ICode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Common {
+   public interface ICode {
+       string Code { get; set; }
+       string Name { get; set; }
+       string Description { get; set; }
+    }
+}
---
+++ ERP/ERP/SCM/IUom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Common;
+
+namespace ERP.SCM {
+    public interface IUom : ICode {
+    }
+}
