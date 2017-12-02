23fe178e5dca954257cc0eacf9164d3467eb1c21
Chad <chad3281@gmail.com>
2017-12-02 17:15:09 -0500

    Reduce directory redundancy
    Root folder now contains the solution (rather than ERP/ERP.sln)

---
+++ ERP/Inventory_Management/InventoryManagement/MRP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryManagement.InventoryManagement
+{
+    // Materials Requirement Planning inventory management strategy:
+    public class MRP : IManagementStrategy
+    {
+        public IInventory Inventory { get; set; }
+    }
+}
